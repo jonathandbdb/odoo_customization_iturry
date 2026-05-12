73b70b82e677d54bddcd6d1f9bed338dac1155eb
jvarela-nexit <jonathan.varela@nexit.com.uy>
2026-05-12 12:34:12 -0500

    [FIX] manifest: agregar author NEXIT y website

--- l10n_pe_edi_fix/__manifest__.py
+++ l10n_pe_edi_fix/__manifest__.py
@@ -24,7 +24,8 @@ a SUNAT/OSE:
    El fix coacciona los valores a `str` antes de ser usados.
     """,
     "category": "Accounting/Localizations/EDI",
-
+    "author": "NEXIT",
+    "website": "https://www.nexit.com.uy",
     "license": "OPL-1",
     "depends": ["l10n_pe_edi"],
     "data": [],
